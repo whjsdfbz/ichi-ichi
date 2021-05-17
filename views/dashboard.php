b51b36a42abd7b1ca6182bc926d499484bc31353
Minami Saito <minamisaito@Minamis-MacBook-Air.local>
2021-05-17 10:22:07 +0900

    first commit
    git branch -M main

---
+++ views/dashboard.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+   <link rel="stylesheet" href="style.css">
+    <title>Dashboard</title>
+    <style>
+        nav{
+
+        }
+        body{
+            font-family: 'Nunito', sans-serif;
+        }
+        .bg_banner-text{
+            background-image: url('production ID_4200101.mp4');
+
+        }  
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md ">
+        <a href="dashboard.php" class="navbar-brand m-auto">
+            <h1><img src="../images/IMG_0606 2.JPG" alt="brand logo" class="rounded-circle" width="50px" height="50px"> Ichi Ichi Japanese</h1>
+        </a> 
+
+        <div class="hamburger-menu">
+            <p class="menubtn"><i class="fas fa-bars"></i></p>
+            <ul>
+                <li class="nav_menu_li"><a href="login.php" >Login </a></li>
+                <li class="nav_menu_li"><a href="event.php">Join event</a></li>
+                <li class="nav_menu_li"><a href="1on1_lesson.php">1on1 Lesson</a></li>
+                <li class="nav_menu_li"><a href="logout.php"></a>Logout</li>
+            </ul> 
+    </div>
+    </nav>
+
+    <!-- <header>
+        <div class="hamburger-menu">
+            <input type="checkbox" id="menu-btn-check">
+            <label for="menu-btn-check" class="menu-btn"><span></span></label>
+            <div class="menu-content">
+                <ul>
+                    <li class="nav_menu_li"><a href="login.php" >Login </a></li>
+                    <li class="nav_menu_li"><a href="event.php">Join event</a></li>
+                    <li class="nav_menu_li"><a href="1on1_lesson.php">1on1 Lesson</a></li>
+                    <li class="nav_menu_li"><a href="logout.php"></a>Logout</li>
+                </ul>
+            </div>
+        </div>
+    </header> -->
+
+       
+        <!-- <ul class="nav navbar-nav ml-auto pr-3">
+            <li class="nav-item">
+                <a href="login.php" class="nav-link"><i class="fas fa-user-alt"></i></a>
+            </li>
+        </ul> --> 
+</nav>
+    
+    <!-- <div class="banner">
+        <div class="video">
+            <video src="../images/Tree - 66836.mp4"　poster="../images/pexels-dagus-pangestu-1440476.jpg" width="100%" autoplay loop muted></video>
+                <div class="banner-text"> 
+                     <div>一期一会</div>
+                    <div>Treasure every meeting</div>
+                </div>
+        </div>      
+    </div>
+     
+   <div class="card">
+
+    </div>
+   
+
+    <footer class="footer footer-expand-md">
+        <a href="https://www.instagram.com/japanese_ichi_ichi/" class="nav-link">
+            <img src="../images/instagram_icon.png" weight="30" height="30" alt="instagram_icon">
+        </a>
+        <a href="https://www.youtube.com/channel/UC2qzvgtmG_Sed-uGCTw6a5g" class="nav-link">
+            <img src="..//images/youtube_social_icon_red.png" weight="30" height="30" alt="youtube_icon">
+        </a>
+        <a href="contact.php" class="nav-link">contact</a>
+    </footer>  -->
+</body>
+</html>
